34312cd5d1bff05ef5e910565fa25a5e7b4523d9
yosukekato165 <padyk165@gmail.com>
2021-12-12 17:20:50 +0900

    remove files and make files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { RecoilRoot } from "recoil";
+
+import { TodoList } from "./components/TodoList";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <>
+    <RecoilRoot>
+      <TodoList />
+    </RecoilRoot>
+  </>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
