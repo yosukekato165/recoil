1494d71ba4f2598226aa1cc7270f428ea8652506
yosukekato165 <padyk165@gmail.com>
2021-12-21 22:18:13 +0900

    get todolist at initial render from mongodb

--- src/recoil/selectors.js
+++ src/recoil/selectors.js
@@ -1,14 +1,27 @@
 import { selector } from "recoil";
 import { todoListState, todoListFilterState } from "./atoms";
+import axios from "axios";
 
 export const filteredTodoListState = selector({
   key: "filteredTodoListState",
-  get: ({ get }) => {
+  get: async ({ get }) => {
     // get() - 他のatom/selectorから値を取得するために使用される関数です。
     // この関数に渡されたすべてのtom/selectorは、暗黙のうちにselectorの依存関係のリストに追加されます。
     // selectorの依存関係のいずれかが変更されると、selectorは再評価されます。
     const filter = get(todoListFilterState);
-    const list = get(todoListState);
+    // const list = get(todoListState);
+
+    const list = await axios
+      .request({
+        method: "get",
+        url: "http://localhost:3030/reference",
+      })
+      .then((res) => {
+        const todoListInMongoDB = res.data;
+        // setTodoList();
+        console.log(todoListInMongoDB);
+        return todoListInMongoDB;
+      });
 
     // todoListFilterStateとtodoListStateをfilteredTodoListStateに接続して、
     // filterの値によってstoreの値をlistから選別している
